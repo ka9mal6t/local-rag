03c7b2964a52ea31f560ad7085ebe94baee0eae5
ka9mal6t <vladimyr.kilko@gmail.com>
2026-09-19 12:25:18 +0200

    add application factory

---
+++ api/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from services.rag_service import RAGService
+from services.static_service import get_static_files_paths
+from database import db
+from .route import main
+from ollama import Client
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+
+    app.client = Client(host="http://ollama:11434")
+    db.init_app(app)
+
+    pdf_files_path = get_static_files_paths(app.root_path, ".pdf", "pdf")
+    app.app_rag = RAGService(app.root_path, pdf_files_path)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(main)
+
+    return app
